b62e34344f47b0e6a10132835d81a961db7f3a14
Marcus Jörgensson <marcusjorgensson@gmail.com>
2022-12-17 08:05:50 +0100

    Chapter1

--- main.go
+++ main.go
@@ -12,6 +12,25 @@ func main() {
   router := gin.Default()
 
   router.GET("/hello", func (c *gin.Context) { c.JSON(http.StatusOK, gin.H{"message": "hello world",})})
+  router.GET("/", indexHandler)
+  router.GET("/:name", nameHandler)
 
   router.Run(":7777")
+}
+
+func indexHandler(c *gin.Context) {
+  c.JSON(200, gin.H{"hej": "yo"})
+}
+
+func nameHandler(c *gin.Context) {
+  name := c.Param("name")
+  name2 := c.Query("name2")
+
+  type msg struct {
+    Name string `json:"name"`
+    name2 string
+  }
+  myMsg := msg{Name: name, name2: name2}
+
+  c.JSON(200, myMsg)
 }
